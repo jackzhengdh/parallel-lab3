b5d36e8fe147a8d9a1539ac7492b4022893c3a72
jackzhengdh <jackzhengdh@gmail.com>
2017-05-04 16:05:26 -0400

    test sample code test1.c

--- test/test1.c
+++ test/test1.c
@@ -108,7 +108,7 @@ int main(int argc, char *argv[]) {
 		cudaFree(dev_num);
 		free(nums);
 		nums = (unsigned int *)malloc(N * sizeof(unsigned int));
-		memcpy(nums, output, N);
+		memcpy(nums, output, sizeof(output));
 		printf("Checking content of nums:\n");
 		for (i = 0; i < N; i++)
 			printf("%u \n", nums[i]);
