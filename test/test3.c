a94468b5089b1b39160cabed1dde009cf0f3fcde
jackzhengdh <jackzhengdh@gmail.com>
2017-05-04 11:48:25 -0400

    test sample code test2.c

--- test/test3.c
+++ test/test3.c
@@ -14,32 +14,44 @@ int main(void) {
 	int *d_a, *d_b, *d_c;		// device copies of a, b, c
 	int size = N * sizeof(int);
 
+	printf("Strating cudaMalloc\n");
 	// Alloc space for device copies of a, b, c
 	cudaMalloc((void **)&d_a, size);
 	cudaMalloc((void **)&d_b, size);
 	cudaMalloc((void **)&d_c, size);
+	printf("Completed cudaMalloc\n");
 
+	printf("Starting malloc\n");
 	// Alloc space for host copies of a, b, c and setup input values
 	a = (int *)malloc(size);
 	b = (int *)malloc(size);
 	c = (int *)malloc(size);
+	printf("Completed malloc\n");
 
+	printf("Starting assigning rands\n");
 	int i;
 	for (i = 0; i < size; i++) {
 		a[i] = rand() % size;
 		b[i] = rand() % size;
 		// printf("%d\n", a[i]);
 	}
+	printf("Completed assigning rands\n");
+
+	printf("Starting copying data from host to device\n");
 	// Copy inputs to device
 	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+	printf("Completed copying data from host to device\n");
 
+	printf("Executing kernal function add\n");
 	// Launch add() kernel on GPU
 	add<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
+	printf("Completed executing kernal function add\n");
 
+	printf("Copying data from device to host\n");
 	// Copy result back to host
 	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
-
+	printf("Completed copying data from device to host\n");
 	
 	printf("%5s %5s %5s\n", "a[]", "b[]", "c[]");
 	for (i = 0; i < size; i++)
